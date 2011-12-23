0c362ff881742fac6b7ead9d757f8493ae5e6e5f
Andrew Aldridge <i80and@gmail.com>
2011-12-23 11:49:33 -0600

    Code shuffle, comments, and a fail() abstraction.

--- quelt-split.c
+++ quelt-split.c
@@ -9,8 +9,17 @@
 #include <zlib.h>
 #include "pprint.h"
 
+// Technically this should be off_t, but we always want it to be 64-bit
+typedef int64_t f_offset;
+// Compatibility shim for Windows
+#ifdef _WIN32
+# define ftello _ftelli64
+#endif
+
+// The command-line option -v sets this to true
 static bool option_verbose = false;
 
+// Our return code is a bitfield
 #define RETURN_OK 0
 #define RETURN_BADARGS 1
 #define RETURN_BADXML 2
@@ -19,17 +28,16 @@ static bool option_verbose = false;
 #define RETURN_INTERNALERROR 16
 static int return_status = RETURN_OK;
 
-// Technically this should be off_t, but we always want this to be 64-bit
-typedef int64_t f_offset;
-// Compatibility shim for Windows
-#ifdef _WIN32
-# define ftello _ftelli64
-#endif
-
-static void set_return_flag(int flag) {
+// Add a flag to our return code
+void set_return_flag(int flag) {
 	if(!(return_status & flag)) {
 		return_status |= flag;
 	}
+}
+
+// Early exit with our return flags
+void fail(void) {
+	exit(return_status);
 }
 
 typedef enum {
@@ -72,6 +80,7 @@ static void write_chunk(ParseCtx* ctx, const XML_Char* s, int len, int flush) {
 	ctx->compression_ctx.next_in = (Bytef*)s;
 	ctx->compression_ctx.avail_in = len;
 
+	// Write until zlib's output buffer is empty
 	do {
 		ctx->compression_ctx.avail_out = chunk_len;
 		ctx->compression_ctx.next_out = buf;
@@ -82,7 +91,7 @@ static void write_chunk(ParseCtx* ctx, const XML_Char* s, int len, int flush) {
 		if(ferror(ctx->dbfile)) {
 			log("Write error");
 			set_return_flag(RETURN_WRITEERROR);
-			exit(return_status);
+			fail();
 		}
 	} while(ctx->compression_ctx.avail_out == 0);
 }
@@ -95,7 +104,7 @@ static void handle_starttag(ParseCtx* ctx, const XML_Char* tag, const XML_Char**
 		if(deflateInit(&ctx->compression_ctx, Z_BEST_COMPRESSION) != Z_OK) {
 			log("Error initializing zlib");
 			set_return_flag(RETURN_INTERNALERROR);
-			exit(return_status);
+			fail();
 		}
 	}
 	else if(strcmp(tag, "title") == 0) {
@@ -143,6 +152,7 @@ void parse(const char* path) {
 	ParseCtx ctx;
 	parsectx_init(&ctx, "quelt.db", "quelt.index");
 
+	// Initialize the XML parser
 	XML_Parser parser = XML_ParserCreate("UTF-8");
 	XML_SetElementHandler(parser,
 		(XML_StartElementHandler)&handle_starttag,
@@ -157,6 +167,7 @@ void parse(const char* path) {
 	const size_t buffer_len = 1024;
 	char buffer[buffer_len];
 
+	// Read chunks from the file and feed them into the XML parser until EOF
 	bool done = false;
 	while(!done) {
 		size_t n_bytes = fread(buffer, sizeof(XML_Char), buffer_len, infile);
@@ -166,7 +177,7 @@ void parse(const char* path) {
 
 		if(!XML_Parse(parser, buffer, n_bytes, done)) {
 			set_return_flag(RETURN_BADXML);
-			exit(return_status);
+			fail();
 		}
 	}
 	
